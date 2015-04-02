a119188fa2b63533414da3e79a6582b5dcf87e3e
Hasnain Baxamoosa <hbaxamoosa@gmail.com>
2015-04-02 11:55:38 -0500

    Updated the activity_main.xml layout to reduce the redundant Card.

--- app/src/main/java/boxofm/materialdesign/MainActivity.java
+++ app/src/main/java/boxofm/materialdesign/MainActivity.java
@@ -63,7 +63,8 @@ public class MainActivity extends Activity
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.container, PlaceholderFragment.newInstance(position + 1)).commit();
+        //fragmentManager.beginTransaction().replace(R.id.container, PlaceholderFragment.newInstance(position + 1)).commit();
+        fragmentManager.beginTransaction().replace(R.id.drawer_layout, PlaceholderFragment.newInstance(position + 1)).commit();
     }
 
     public void onSectionAttached(int number) {
